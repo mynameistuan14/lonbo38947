5a394236ae1d31db00d6416f3fd6b8a15f1eca8a
mynameistuan14 <106077288+mynameistuan14@users.noreply.github.com>
2024-05-20 16:40:41 +0700

    Update tra.sh

--- tra.sh
+++ tra.sh
@@ -6,7 +6,7 @@ while true; do
   $command 2>&1 |
   while IFS= read -r line; do
     echo "$line"
-    if echo "$line" | grep -q "sendRequest"; then
+    if echo "$line" | grep -q "keepalive"; then
       echo "Specific string found in the output. Terminating and re-executing the command."
 
       child_pid=$(pgrep -f "$command")
